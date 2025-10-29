3cddba239c01f3661f64093a3dddca4d594a9b6a
AnushervonKhidirov <ankhid98@gmail.com>
2025-10-29 22:21:27 +0500

    service imported

--- src/queue/queue.module.ts
+++ src/queue/queue.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { QueueService } from './queue.service';
 import { QueueController } from './queue.controller';
+import { ServiceService } from 'src/service/service.service';
 import { PrismaService } from 'src/prisma/prisma.service';
 
 @Module({
-  providers: [QueueService, PrismaService],
+  providers: [QueueService, ServiceService, PrismaService],
   controllers: [QueueController],
 })
 export class QueueModule {}
