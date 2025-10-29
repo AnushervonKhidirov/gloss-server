0d576a0da67753cdf2cef536545ec5b5cf7ed3b6
AnushervonKhidirov <ankhid98@gmail.com>
2025-10-29 17:28:16 +0500

    prisma error handler added

--- src/common/helper/exception-handler.helper.ts
+++ src/common/helper/exception-handler.helper.ts
@@ -1,7 +1,22 @@
-import { HttpException, InternalServerErrorException } from '@nestjs/common';
+import {
+  BadRequestException,
+  ConflictException,
+  HttpException,
+  InternalServerErrorException,
+  NotFoundException,
+} from '@nestjs/common';
+import {
+  PrismaClientKnownRequestError,
+  PrismaClientValidationError,
+} from 'generated/prisma/runtime/library';
 
 export function exceptionHandler(err: unknown): [null, HttpException] {
   if (err instanceof HttpException) return [null, err];
+  if (isPrismaError(err)) return [null, prismaErrHandler(err)];
+
+  if (err instanceof PrismaClientValidationError) {
+    return [null, new BadRequestException()];
+  }
 
   return [
     null,
@@ -9,4 +24,20 @@ export function exceptionHandler(err: unknown): [null, HttpException] {
       'Something went wrong, please try again later',
     ),
   ];
+}
+
+function prismaErrHandler(err: PrismaClientKnownRequestError) {
+  const Error: {
+    [key: PrismaClientKnownRequestError['code']]: HttpException;
+  } = {
+    P2025: new NotFoundException(err.meta?.cause),
+    P2002: new ConflictException(err.meta?.cause),
+  };
+
+  if (!Error[err.code]) return new InternalServerErrorException();
+  return Error[err.code];
+}
+
+function isPrismaError(err: unknown) {
+  return err instanceof PrismaClientKnownRequestError;
 }
