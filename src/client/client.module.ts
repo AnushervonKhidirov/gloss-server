7640ae2a5b858401fe49f086429d99a9f748b1de
AnushervonKhidirov <ankhid98@gmail.com>
2025-11-28 17:01:00 +0500

    clients module fixed

--- src/client/client.module.ts
+++ src/client/client.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
+import { TokenModule } from 'src/token/token.module';
 import { ClientService } from './client.service';
 import { ClientController } from './client.controller';
 import { PrismaService } from 'src/prisma/prisma.service';
 
 @Module({
+  imports: [TokenModule],
   providers: [ClientService, PrismaService],
   controllers: [ClientController],
 })
