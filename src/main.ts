b69d005c03357a90502389e0f2888b07d1aa8a19
AnushervonKhidirov <ankhid98@gmail.com>
2025-11-29 15:53:06 +0500

    exception filter added

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,11 @@
-import { NestFactory } from '@nestjs/core';
+import { HttpAdapterHost, NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { HttpExceptionFilter } from './common/exception-filter/exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
+  const { httpAdapter } = app.get(HttpAdapterHost);
+  app.useGlobalFilters(new HttpExceptionFilter(httpAdapter));
   await app.listen(process.env.PORT ?? 3000);
 }
 
